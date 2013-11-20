756d134b3d9cde536bb3d6ba300875781bf3ac8a
Bastian <Bastian@localhost>
2013-11-20 10:06:50 +0100

    Designed the code so it only works with 32bit float images. Provided a function to convert images to 32bit float. Added some comments. Added a 16bit test image.

--- main.cpp
+++ main.cpp
@@ -3,37 +3,55 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+//Applies a filter mask to a 32bit float image
+//Mind that the values after applying the filter may be greater than 1 and negative
 void applyHighpassFilter(cv::Mat& img){
-	cv::Mat mask = (cv::Mat_<char>(3, 3) <<	 0, 0, 0, 
-											-1, 0, 1, 
-											 0, 0, 0);
+	CV_Assert(img.depth() == CV_32F);
+	cv::Mat mask = (cv::Mat_<char>(3, 3) <<	-1, 0, 1, 
+											-2, 0, 2, 
+											-1, 0, 1);
 	cv::filter2D(img, img, img.depth(), mask, cv::Point(-1, -1), 0, cv::BORDER_REPLICATE);
 }
 
+//Applies a ramp filter to a 32bit float image with 1 channel; weights the center less than the borders (in horizontal direction)
 void applyRampFilter(cv::Mat& img){
-	//CV_Assert(img.depth() != sizeof(uchar));
 	CV_Assert(img.channels() == 1);
-	
-	const double minAttenuation = 1;
+	CV_Assert(img.depth() == CV_32F);
+
+	const double minAttenuation = 1;	//the values for the highest and lowest weight
 	const double maxAttenuation = 0;
 
 	int r = img.rows;
 	int c = img.cols;
 
 	double centerColumn = (double)img.cols / 2;
-	uchar* ptr;
+	double factor;
+	double inverseFactor;
+	float* ptr;
 	for(int i = 0; i < r; ++i){
-		ptr = img.ptr<uchar>(i);
-		double actualColumn = -centerColumn;
+		ptr = img.ptr<float>(i);
 		for(int j = 0; j < c; ++j){
-			ptr[j] = uchar((double)ptr[j] * ((abs(actualColumn) / centerColumn) * minAttenuation + ((centerColumn - abs(actualColumn) )/centerColumn) * maxAttenuation));
-			++actualColumn;
+			factor = abs(j - centerColumn) / centerColumn;
+			inverseFactor = 1 - factor;
+			ptr[j] = ptr[j] * (factor * minAttenuation + inverseFactor * maxAttenuation);
 		}
 	}
 }
 
+//converts an image to 32 bit float
+//only unsigned types are allowed as input
+void convertTo32bit(cv::Mat& img){
+	CV_Assert(img.depth() == CV_8U || img.depth() == CV_16U || img.depth() == CV_32F);
+	if (img.depth() == CV_8U){
+		img.convertTo(img, CV_32F, 1.0 / (float)pow(2, 8));
+	} else if (img.depth() == CV_16U){
+		img.convertTo(img, CV_32F, 1.0 / (float)pow(2, 16));
+	}
+}
+
 int main(){
-	cv::Mat image = cv::imread("../sourcefiles/data/uncompressed.tif", CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat image = cv::imread("../sourcefiles/data/uncompressed16.tif", CV_LOAD_IMAGE_ANYDEPTH);
+	convertTo32bit(image);
 	imshow("Image", image);
 	applyHighpassFilter(image);
 	imshow("Filtered", image);
