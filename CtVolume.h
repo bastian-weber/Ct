3ce1ae6bf9ab877dd47f50f993c995ba8f2ccf38
Bastian <Bastian@localhost>
2014-01-03 14:10:45 +0100

    Some smaller changes.

--- CtVolume.h
+++ CtVolume.h
@@ -12,14 +12,15 @@ public:
 	CtVolume();										//constructor 1
 	CtVolume(std::string path);						//constructor 2
 	void sinogramFromImages(std::string path);		//creates a sinogram from the images in the specified path
-	void displaySinogram();							//lets the user scroll through the images in the sinogram	
+	void displaySinogram() const;							//lets the user scroll through the images in the sinogram	
 private:
 	//variables
 	std::vector<cv::Mat> sinogram;					//here the images are stored
-	int currentlyDisplayedImage = 0;				//holds the index of the image that is currently being displayed
+	std::vector<float> volume;						//holds the reconstructed volume
+	mutable int currentlyDisplayedImage = 0;		//holds the index of the image that is currently being displayed
 	//functions			
-	void handleKeystrokes();						//handles the forward and backward arrow keys when sinogram is displayed
-	void convertTo32bit(cv::Mat& img);				//converts an image to 32bit float
-	void applyRampFilter(cv::Mat& img);				//applies the ramp filter to an image
-	void applyHighpassFilter(cv::Mat& img);			//applies the highpass filter to an image
+	void handleKeystrokes() const;						//handles the forward and backward arrow keys when sinogram is displayed
+	void convertTo32bit(cv::Mat& img) const;				//converts an image to 32bit float
+	void applyRampFilter(cv::Mat& img) const;				//applies the ramp filter to an image
+	void applyHighpassFilter(cv::Mat& img) const;			//applies the highpass filter to an image
 };
