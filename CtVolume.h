9776d64bdb21800dc7a13d9d8b6832ce31ae892a
Bastian <17008429+bastian-weber@users.noreply.github.com>
2014-01-19 09:33:15 +0100

    Added ability to read angles from csv file

--- CtVolume.h
+++ CtVolume.h
@@ -13,19 +13,27 @@
 #include <opencv2/imgproc/imgproc.hpp>							//image processing functionality of OpenCV (filter masks etc)
 #include "dirent.h"												//library for accessing the filesystem						
 
+struct Projection{
+	Projection();
+	Projection(cv::Mat image, double angle);					//Constructor
+	cv::Mat image;												
+	double angle;
+};
+
 class CtVolume{
 public:
 	//functions		
 	CtVolume();													//constructor 1
-	CtVolume(std::string path);									//constructor 2
+	CtVolume(std::string folderPath, std::string csvPath);		//constructor 2
 	enum ThreadingType{SINGLETHREADED, MULTITHREADED};
-	void sinogramFromImages(std::string path);					//creates a sinogram from the images in the specified path
+	void sinogramFromImages(std::string folderPath,				//creates a sinogram from the images in the specified path
+							std::string csvPath);					
 	void displaySinogram() const;								//lets the user scroll through the images in the sinogram	
 	void reconstructVolume(ThreadingType threading);				//reconstructs the 3d-volume from the sinogram
 	void saveVolumeToBinaryFile(std::string filename) const;	//saves the reconstructed volume to a binary file
 private:
 	//variables		
-	std::vector<cv::Mat> _sinogram;								//here the images are stored
+	std::vector<Projection> _sinogram;							//here the images are stored
 	std::vector<std::vector<std::vector<float>>> _volume;		//holds the reconstructed volume
 	mutable int _currentlyDisplayedImage;						//holds the index of the image that is currently being displayed								
 	mutable int _xSize;											//the size of the volume in x, y and z direction, is calculated
@@ -34,7 +42,9 @@ private:
 	mutable int _imageWidth;									//stores the height and width of the images in the sinogram
 	mutable int _imageHeight;									//assigned when sinogram is created
 	mutable std::mutex _volumeMutex;							//prevents that two threads access the volume simultaneously
-	//functions					
+	//functions			
+	bool readCSV(std::string filename,							//reads the additional information from the csv file
+				 std::vector<double>& result) const;				
 	void handleKeystrokes() const;								//handles the forward and backward arrow keys when sinogram is displayed
 	void convertTo32bit(cv::Mat& img) const;					//converts an image to 32bit float
 	void applyRampFilter(cv::Mat& img) const;					//applies the ramp filter to an image
@@ -42,7 +52,8 @@ private:
 	void reconstructionThread(cv::Point3i lowerBounds, 
 							  cv::Point3i upperBounds, 
 							  double deltaBeta, 
-							  double D);				
+							  double D,
+							  bool consoleOutput);
 	float bilinearInterpolation(double u,						//interpolates bilinear between those four intensities
 								double v,				
 								float u0v0, 
