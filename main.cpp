03e4ef81e779c56ce3e2d72d5676e4deffce90e6
Bastian <17008429+bastian-weber@users.noreply.github.com>
2014-03-16 13:27:49 +0100

    Corrected u-offset correction.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <Windows.h>
 #include "CtVolume.h"
 
 int main(){
-	CtVolume myVolume("G:/Desktop/Turnschuh2/angles.csv", CtVolume::RAMLAK);
-	myVolume.displaySinogram(true);	
+	//if (!SetPriorityClass(GetCurrentProcess(), PROCESS_MODE_BACKGROUND_BEGIN)){
+	//	std::cout << "could not set the priority";
+	//}
+	CtVolume myVolume("G:/Desktop/Turnschuh_256/angles.csv", CtVolume::RAMLAK);
+	//myVolume.displaySinogram(true);	
 	myVolume.reconstructVolume(CtVolume::MULTITHREADED);
 	myVolume.saveVolumeToBinaryFile("G:/Desktop/volume.raw");
 	return 0;
