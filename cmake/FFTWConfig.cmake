155d46943ba492e574e70131c2bce12d04f0fb51
Bastian Weber <bastian.weber@uni-weimar.de>
2015-04-25 14:06:50 +0200

    Corrected FFTWConfig.cmake.

--- cmake/FFTWConfig.cmake
+++ cmake/FFTWConfig.cmake
@@ -1,5 +1,6 @@
 		if(WIN32)
-			set(FFTW_LIB_NAME "libfftw3f-3.lib")
+			set(FFTW_LIB_NAME "libfftw3f-3.dll")
+			SET(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll")
 		elseif(UNIX)
 			set(FFTW_LIB_NAME "libfftw3.a")
 		endif()
@@ -12,27 +13,23 @@
 
 			add_library(fftw SHARED IMPORTED)
 			set_property(TARGET fftw APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+			set_property(TARGET fftw APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)		
 			set_target_properties(fftw PROPERTIES
-			  IMPORTED_IMPLIB_RELEASE "${FFTW_LIB}"
-			  )
-
-			set_property(TARGET fftw APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
-			set_target_properties(fftw PROPERTIES
-			  IMPORTED_IMPLIB_DEBUG "${FFTW_LIB}"
-			  )
-
-			set_property(TARGET fftw PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${FFTW_INCLUDE_DIR})
+								  INTERFACE_INCLUDE_DIRECTORIES ${FFTW_INCLUDE_DIR}
+								  IMPORTED_LOCATION_RELEASE "${FFTW_LIB}"
+								  IMPORTED_LOCATION_DEBUG "${FFTW_LIB}")
 
 			if(WIN32)
-				FIND_FILE(FFTW_DLL libfftw3f-3.dll)
+				FIND_LIBRARY(FFTW_IMPLIB libfftw3f-3.lib)
 
-				if(NOT ${FFTW_DLL} STREQUAL "FFTW_DLL-NOTFOUND")
-					set_target_properties(fftw PROPERTIES 
-					IMPORTED_LOCATION_RELEASE "${FFTW_DLL}"
-					IMPORTED_LOCATION_DEBUG "${FFTW_DLL}")
+				if(NOT ${FFTW_IMPLIB} STREQUAL "FFTW_IMPLIB-NOTFOUND")
+					set_target_properties(fftw PROPERTIES
+					  					  IMPORTED_IMPLIB_RELEASE "${FFTW_IMPLIB}"
+					  					  IMPORTED_IMPLIB_DEBUG "${FFTW_IMPLIB}")
 				else()
 					message("FFTW dll could not be found.")
 				endif()
+
 			endif()
 
 		else()
