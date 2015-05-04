c6c501bd7fddd3fb3aa3bbac76bbf7cd2f72f76b
Bastian Weber <17008429+bastian-weber@users.noreply.github.com>
2015-05-04 12:21:21 +0200

    Performance improvements.

--- CtVolume.h
+++ CtVolume.h
@@ -21,12 +21,12 @@
 
 //for std::numeric_limits<std::streamsize>::max()
 #undef max
-			
+
 //struct for storing one projection
-struct Projection{			
-	Projection();			
+struct Projection{
+	Projection();
 	Projection(cv::Mat image, double angle, double heightOffset);			//Constructor
-	cv::Mat image;												
+	cv::Mat image;
 	double angle;
 	double heightOffset;													//for random trajectory
 };
@@ -34,34 +34,34 @@ struct Projection{
 //The actual reconstruction class
 class CtVolume{
 public:
-	enum FilterType{RAMLAK, HANN, RECTANGLE};
+	enum FilterType{ RAMLAK, HANN, RECTANGLE };
 	//functions		
 	CtVolume();																//constructor 1
-	CtVolume(std::string csvFile,										
+	CtVolume(std::string csvFile,
 			 CtVolume::FilterType filterType = CtVolume::RAMLAK);
 	void sinogramFromImages(std::string csvFile,							//creates a sinogramm out of images specified in csvFile, filterType specifies the prefilter
 							CtVolume::FilterType filterType = CtVolume::RAMLAK);
 	void displaySinogram(bool normalize = false) const;						//lets the user scroll through the images in the sinogram, set normalize for normalizing the gray values	
 	void reconstructVolume();												//reconstructs the 3d-volume from the sinogram
 	void saveVolumeToBinaryFile(std::string filename) const;				//saves the reconstructed volume to a binary file
-private:			
+private:
 	//variables					
 	std::vector<Projection> _sinogram;										//here the images are stored
 	std::vector<std::vector<std::vector<float>>> _volume;					//holds the reconstructed volume
 	mutable int _currentlyDisplayedImage;									//holds the index of the image that is currently being displayed								
 	mutable int _xSize;														//the size of the volume in x, y and z direction, is calculated
 	mutable int _ySize;														//when sinogram is created
-	mutable int _zSize;			
+	mutable int _zSize;
 	mutable int _imageWidth;												//stores the height and width of the images in the sinogram
 	mutable int _imageHeight;												//assigned when sinogram is created
-	mutable std::pair<float, float> _minMaxValues;						
+	mutable std::pair<float, float> _minMaxValues;
 	double _SD;																//the distance of the source to the detector in pixel
 	double _SO;																//the distance of the source to the object in pixel
 	double _uOffset;														//the offset of the rotation axis in u direction
 	//functions						
 	std::pair<float, float> getSinogramMinMaxIntensity() const;				//returns the highest and lowest density value out of all images in the sinogram
 	cv::Mat normalizeImage(cv::Mat const& image,							//returns a new image which is a version of the old image that is normalized by min and max value
-						   float minValue, 
+						   float minValue,
 						   float maxValue) const;
 	void handleKeystrokes(bool normalize) const;							//handles the forward and backward arrow keys when sinogram is displayed
 	void imagePreprocessing(CtVolume::FilterType filterType);				//applies the necessary filters to the images prior to the reconstruction
@@ -76,25 +76,25 @@ private:
 	double hannWindowFilter(double n, double N) const;						//fourier filters for each n out of N
 	double rectangleWindowFilter(double n, double N) const;
 	void applyFourierHighpassFilter2D(cv::Mat& image) const;				//applies a highpass filter in the frequency domain (2D) (not used)
-	void reconstructionCore();									//does the actual reconstruction
-	float bilinearInterpolation(double u,									//interpolates bilinear between those four intensities
-								double v,							
-								float u0v0,				
-								float u1v0,				
-								float u0v1,				
-								float u1v1) const;			
+	void reconstructionCore();												//does the actual reconstruction
+	float bilinearInterpolation(double u,							//interpolates bilinear between those four intensities
+									   double v,
+									   float u0v0,
+									   float u1v0,
+									   float u0v1,
+									   float u1v1) const;
 	double W(double D, double u, double v) const;							//weight function for the reconstruction of the volume
 	//coordinate transformation functions			
 	double worldToVolumeX(double xCoord) const;								//coordinate transformations from the coordinates of the vector to
 	double worldToVolumeY(double yCoord) const;								//the coordinates of the "world" and the other way around
-	double worldToVolumeZ(double zCoord) const;			
-	double volumeToWorldX(double xCoord) const;			
-	double volumeToWorldY(double yCoord) const;			
-	double volumeToWorldZ(double zCoord) const;			
+	double worldToVolumeZ(double zCoord) const;
+	double volumeToWorldX(double xCoord) const;
+	double volumeToWorldY(double yCoord) const;
+	double volumeToWorldZ(double zCoord) const;
 	double imageToMatU(double uCoord)const;									//coordinate transformations from the coordinates of the image
 	double imageToMatV(double vCoord)const;									//to the coordinates of the saved matrix (always starting at 0)
-	double matToImageU(double uCoord)const;									
-	double matToImageV(double vCoord)const;				
+	double matToImageU(double uCoord)const;
+	double matToImageV(double vCoord)const;
 	int fftCoordToIndex(int coord, int size) const;							//coordinate transformation for the FFT lowpass filtering
-																			//only used for the 2D highpass filtering, which is currently not used
+	//only used for the 2D highpass filtering, which is currently not used
 };
