cc9c48f77b27738c7babc5ff0c99845319c61975
Bastian Weber <17008429+bastian-weber@users.noreply.github.com>
2015-07-24 20:23:46 +0200

    Updated cmake to use macro for getting library file paths.

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -1,3 +1,5 @@
+########################################### Copy DLLS for passed modules ###########################################
+
 macro(copydlls MODULELIST)
 	foreach(ELEMENT ${MODULELIST})
 		get_target_property(LOC_R ${ELEMENT} LOCATION_RELEASE)
@@ -48,6 +50,54 @@ macro(copydlls MODULELIST)
 	endforeach()
 endmacro()
 
+########################################### Hides the specified variable from the gui ###########################################
+
 macro(hide_from_gui VARIABLE)
 	set(${VARIABLE} ${${VARIABLE}} CACHE INTERNAL "hidden" FORCE)
+endmacro()
+
+########################################### Asks for a path to the corresponding library ###########################################
+
+# sets ${LIBNAME}_ROOT_FOUND (all uppercase) true if the desired file was found in the selected directory
+# PATH_${LIBNAME}_ROOT (all uppercase) contains the selected path
+
+macro (ask_for_path LIBNAME WINDOWS_DEFAULT_PATH LINUX_DEFAULT_PATH FILE_SEARCHED PATH_HINTS)
+	string(TOUPPER ${LIBNAME} UPPERLIBNAME)
+	if (NOT DEFINED PATH_${UPPERLIBNAME}_ROOT)
+
+		if(WIN32)
+			set(PATH_${UPPERLIBNAME}_ROOT ${WINDOWS_DEFAULT_PATH} CACHE PATH "${LIBNAME} root directory")
+		elseif(UNIX)
+			set(PATH_${UPPERLIBNAME}_ROOT ${LINUX_DEFAULT_PATH} CACHE PATH "${LIBNAME} root directory")
+		endif(WIN32)
+
+	endif()
+
+	if("${PATH_${UPPERLIBNAME}_ROOT}" STREQUAL "")
+
+		message("Please specify the ${LIBNAME} root directory (PATH_${UPPERLIBNAME}_ROOT).")
+
+	else()
+
+		unset(${UPPERLIBNAME}_TMP CACHE)
+
+		find_path(${UPPERLIBNAME}_TMP 
+		NAMES ${FILE_SEARCHED}
+		HINTS ${${PATH_HINTS}})	
+
+		hide_from_gui(${UPPERLIBNAME}_TMP)
+
+		if(${${UPPERLIBNAME}_TMP} STREQUAL "${UPPERLIBNAME}_TMP-NOTFOUND")
+
+			message("The path you specified as ${LIBNAME} root directory seems to be incorrect. Please chosse again.")
+
+		else()
+
+			set(PATH_${UPPERLIBNAME}_ROOT ${${UPPERLIBNAME}_TMP} CACHE PATH "${LIBNAME} root directory" FORCE)
+			set(${UPPERLIBNAME}_ROOT_FOUND ON)
+			hide_from_gui(${UPPERLIBNAME}_ROOT_FOUND)
+
+		endif()
+
+	endif()
 endmacro()
