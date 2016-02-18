42509f01b496743c2113e92d09cb568d5788c64e
Bastian Weber <17008429+bastian-weber@users.noreply.github.com>
2016-02-18 11:14:34 +0100

    Added comments and organised code.

--- cuda_ct.h
+++ cuda_ct.h
@@ -17,15 +17,29 @@ namespace ct {
 
 	namespace cuda {
 
-		size_t getFreeMemory();
-		int getMultiprocessorCnt(int deviceId);
-		int getMemoryBusWidth(int deviceId);
-		void applyFrequencyFiltering(cv::cuda::PtrStepSz<float2> image, int filterType, cudaStream_t stream, bool& success);
-		void applyFeldkampWeightFiltering(cv::cuda::PtrStepSz<float> image, float SD, float matToImageUPreprocessed, float matToImageVPreprocessed, cudaStream_t stream, bool& success);
-		cudaPitchedPtr create3dVolumeOnGPU(size_t xSize, size_t ySize, size_t zSize, bool& success);
-		void delete3dVolumeOnGPU(cudaPitchedPtr devicePtr, bool& success);
-		std::shared_ptr<float> download3dVolume(cudaPitchedPtr devicePtr, size_t xSize, size_t ySize, size_t zSize, bool& success);
-		void startReconstruction(cv::cuda::PtrStepSz<float> image,
+		size_t getFreeMemory();														//returns the free memory on the currently active device
+		int getMultiprocessorCnt(int deviceId);										//returns the amount of streaming processors
+		int getMemoryBusWidth(int deviceId);										//returns the global memory bus width
+		void applyFrequencyFiltering(cv::cuda::PtrStepSz<float2> image,				//applies a frequency filter on a dft spectrum
+									 int filterType, 
+									 cudaStream_t stream, 
+									 bool& success);
+		void applyFeldkampWeightFiltering(cv::cuda::PtrStepSz<float> image,			//applies the feldkamp weighting to an image
+										  float SD, 
+										  float matToImageUPreprocessed, 
+										  float matToImageVPreprocessed, 
+										  cudaStream_t stream, 
+										  bool& success);
+		cudaPitchedPtr create3dVolumeOnGPU(size_t xSize, size_t ySize,				//allocates a 3d volume on the gpu and initialises it with 0
+										   size_t zSize, 
+										   bool& success);
+		void delete3dVolumeOnGPU(cudaPitchedPtr devicePtr, bool& success);			//deletes the 3d volume from the gpu memory
+		std::shared_ptr<float> download3dVolume(cudaPitchedPtr devicePtr, 
+												size_t xSize, 
+												size_t ySize, 
+												size_t zSize, 
+												bool& success);
+		void startReconstruction(cv::cuda::PtrStepSz<float> image,					//starts the reconstruction
 								 cudaPitchedPtr volumePtr,
 								 size_t xSize,
 								 size_t ySize,
